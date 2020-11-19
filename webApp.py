2aaf24fec8639e8baeb3fe5e95cb8c60239f8522
mohanmohadikar <mohadikar.mohan@sitpune.edu.in>
2020-11-19 15:30:32 +0530

    Update webApp.py

--- webApp.py
+++ webApp.py
@@ -18,6 +18,7 @@ import uuid
 import flask
 import urllib
 from PIL import Image
+import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
@@ -151,27 +152,27 @@ def success():
 
 
 def extract_features(filename, model):
-        try:
-            image = Image.open(filename)
+    try:
+        image = Image.open(filename)
             
-        except:
-            print("ERROR: Couldn't open image! Make sure the image path and extension is correct")
-        image = image.resize((299,299))
-        image = np.array(image)
-        # for images that has 4 channels, we convert them into 3 channels
-        if image.shape[2] == 4: 
-            image = image[..., :3]
-        image = np.expand_dims(image, axis=0)
-        image = image/127.5
-        image = image - 1.0
-        feature = model.predict(image)
-        return feature
+    except:
+        print("ERROR: Couldn't open image! Make sure the image path and extension is correct")
+    image = image.resize((299,299))
+    image = np.array(image)
+    # for images that has 4 channels, we convert them into 3 channels
+    if image.shape[2] == 4: 
+        image = image[..., :3]
+    image = np.expand_dims(image, axis=0)
+    image = image/127.5
+    image = image - 1.0
+    feature = model.predict(image)
+    return feature
 
 def word_for_id(integer, tokenizer):
- for word, index in tokenizer.word_index.items():
-     if index == integer:
-         return word
- return None
+ 	for word, index in tokenizer.word_index.items():
+    	 if index == integer:
+        	return word
+ 	return None
 
 
 def generate_desc(model, tokenizer, photo, max_length):
