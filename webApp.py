bc2b2632073cad8e8116f130598f95b9f131aa07
mohanmohadikar <mohadikar.mohan@sitpune.edu.in>
2020-11-21 21:47:31 +0530

    Update webApp.py

--- webApp.py
+++ webApp.py
@@ -20,7 +20,7 @@ import urllib
 from PIL import Image
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.applications.xception import Xception
